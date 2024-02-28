a843b8cb6742bba8b7883f8e7ebf676e7780f10d
manoj-browserstack <manoj.m@browserstack.com>
2024-02-28 17:18:43 +0530

    repo

---
+++ src/test/java/com/browserstack/stepdefs/StackDemoSteps.java
@@ -0,0 +1,64 @@
+package com.browserstack.stepdefs;
+
+import com.browserstack.pageobjects.HomePage;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+
+import org.testng.Assert;
+
+public class StackDemoSteps {
+    private WebDriver driver;
+    private HomePage homePage;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        MutableCapabilities capabilities = new MutableCapabilities();
+        HashMap<String, String> bstackOptions = new HashMap<>();
+        bstackOptions.putIfAbsent("source", "cucumber-java:sample-master:v1.2");
+        capabilities.setCapability("bstack:options", bstackOptions);
+        driver = new RemoteWebDriver(
+                new URL("https://hub.browserstack.com/wd/hub"), capabilities);
+        homePage = new HomePage(driver);
+    }
+
+    @Given("^I am on the website '(.+)'$")
+    public void I_am_on_the_website(String url) throws Throwable {
+        driver.get(url);
+        Thread.sleep(2000);
+    }
+
+    @When("^I select a product and click on 'Add to cart' button")
+    public void I_select_a_product_and_add_to_cart() throws Throwable {
+        homePage.selectFirstProductName();
+        homePage.clickAddToCartButton();
+        Thread.sleep(2000);
+    }
+
+    @Then("the product should be added to cart")
+    public void product_should_be_added_to_cart() {
+        homePage.waitForCartToOpen();
+        Assert.assertEquals(homePage.getSelectedProductName(), homePage.getProductCartText());
+    }
+
+    @Then("the page title should contain '(.+)'$")
+    public void page_title_should_contain(String expectedTitle) {
+        Assert.assertTrue(driver.getTitle().contains(expectedTitle));
+    }
+
+    @After
+    public void teardown(Scenario scenario) throws Exception {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
---
+++ src/test/java/failedTestCases/Retry.java
@@ -0,0 +1,21 @@
+package failedTestCases;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class Retry implements IRetryAnalyzer {
+
+    int maxTry = 3;
+    int count = 0;
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if(!result.isSuccess()){
+            if(count < maxTry){
+                count ++;
+                return true;
+            }
+        }
+        return false;
+    }
+}
